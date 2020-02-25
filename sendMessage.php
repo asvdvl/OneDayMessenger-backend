54ddd0eb87c7fb31a2cc5e77db4b589fd98b318b
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-02-25 21:49:26 +0300

    complete parse input data

--- sendMessage.php
+++ sendMessage.php
@@ -19,5 +19,51 @@ function setError($errorCode)
     }
 }
 
+###получение данных от клиента
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+	if (isset($_POST['user_uid']))
+	{
+        if(strlen($_POST['user_uid']) == 40)
+        {
+            $received_data_from_client['user_uid'] = $_POST['user_uid'];
+        }
+        else
+        {
+            setError(3);
+        }
+	}
+	else
+	{
+		setError(1);
+    }
 
-?>
+
+    if (isset($_POST['messadeBody']))
+	{
+        if(strlen($_POST['messadeBody']) <= 256)
+        {
+            $received_data_from_client['messadeBody'] = $_POST['messadeBody'];
+        }
+        else
+        {
+            setError(3);
+        }
+	}
+	else
+	{
+		setError(1);
+    }
+}
+else
+{
+	setError(2);
+}
+
+/*
+yes:parse post data 
+ no:connect to bd 
+ no:check valid user
+ no:add message in db 
+*/
+ ?>
