7a8402e08f63d7413de74b745497236b5ca25ea1
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-02-25 11:11:38 +0300

    add workers arrays|add set error function

--- sendMessage.php
+++ sendMessage.php
@@ -1,3 +1,23 @@
 <?php
+//header('Content-Type: application/json');
+$received_data_from_client = [
+    'user_uid' => '',
+    'messadeBody' => ''
+    ];
+        
+$send_data_to_client = [
+    'error' => '0',
+    'APIVersion' => 'v0.0'
+    ];    
+
+function setError($errorCode)
+{
+    global $send_data_to_client;
+    if ($send_data_to_client['error'] == "0")
+    {
+        $send_data_to_client['error'] = (string)$errorCode;
+    }
+}
+
 
 ?>
