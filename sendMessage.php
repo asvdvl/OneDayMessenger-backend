02e9c95445c4dbc4a709081cc7dbe7a15df211d7
asvdeveloper <57688070+asvdeveloper@users.noreply.github.com>
2020-02-26 11:58:51 +0300

    Fix content type

--- sendMessage.php
+++ sendMessage.php
@@ -1,5 +1,5 @@
 <?php
-//header('Content-Type: application/json');
+header('Content-Type: application/json');
 $received_data_from_client = [
     'user_id' => '',
     'user_uid' => '',
@@ -36,6 +36,83 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 	}
 	else
 	{
+		setError(1);
+    }
+
+
+    if (isset($_POST['messadeBody']))
+	{
+        if(strlen($_POST['messadeBody']) <= 256)
+        {
+            $received_data_from_client['messadeBody'] = $_POST['messadeBody'];
+        }
+        else
+        {
+            setError(3);
+        }
+	}
+	else
+	{
+		setError(1);
+    }
+}
+else
+{
+	setError(2);
+}
+
+###подкл. к бд
+try
+{
+	$bd_link = mysqli_connect("localhost", "messenger_backend_worker", "B9Z1VPNuvljoGTcm", "messenger_db");
+}
+catch (Exception $e)
+{
+	setError("100.".mysqli_connect_errno());
+}
+
+###поиск 
+try
+{
+	$sql_query = "SELECT `user_id` FROM `profile_users_data` WHERE `user_uid` = '".$received_data_from_client['user_uid']."'";
+	$result = mysqli_query($bd_link, $sql_query);
+	$result_parsing = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+	if($result_parsing)
+	{
+		$received_data_from_client['user_id'] = $result_parsing['user_id'];
+	}
+	else
+	{
+        setError(201);
+	}
+}
+catch (Exception $e)
+{
+	setError("101.".mysqli_connect_errno());
+}
+
+###Добавление сообщения 
+if ($received_data_from_client['error'] == 0) {
+    try
+    {
+	    $sql_query = "INSERT INTO `messages_data` (`message_uid`, `user_id`, `time`, `message_text`) VALUES (NULL, '".$received_data_from_client['user_id']."', current_timestamp(), '".$received_data_from_client['messadeBody']."')";
+	    $result = mysqli_query($bd_link, $sql_query);
+    
+	    if(!$result)
+	    {
+		    setError(104);
+	    }
+    }
+    catch (Exception $e)
+    {
+	    setError("105.".mysqli_connect_errno());
+    }
+}
+
+$ext = (string)json_encode($send_data_to_client);
+echo $ext;
+ ?>
 		setError(1);
     }
 
